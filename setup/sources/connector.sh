f5aae42dc820662a57cb01dbfa0175f7089b188d
Rosy <ihda455@gmail.com>
2025-04-09 14:33:19 +0700

    init

--- setup/sources/connector.sh
+++ setup/sources/connector.sh
@@ -5,30 +5,34 @@ set -a
 source ./setup/sources/.env
 set +a
 
-echo "Registering Debezium connector for database: $SRC_POSTGRES_DB"
+echo "🔌 Registering Debezium connector for database: $SRC_POSTGRES_DB"
 
 curl -X POST http://localhost:8084/connectors \
   -H "Content-Type: application/json" \
-  -d "{
-    \"name\": \"postgres-source-connector\",
-    \"config\": {
-      \"connector.class\": \"io.debezium.connector.postgresql.PostgresConnector\",
-      \"plugin.name\": \"pgoutput\",
-      \"database.hostname\": \"sources\",
-      \"database.port\": \"5432\",
-      \"database.user\": \"$SRC_POSTGRES_USER\",
-      \"database.password\": \"$SRC_POSTGRES_PASSWORD\",
-      \"database.dbname\": \"$SRC_POSTGRES_DB\",
-      \"database.server.name\": \"pgserver\",
-      \"slot.name\": \"debezium\",
-      \"publication.name\": \"dbz_publication\",
-      \"table.include.list\": \"production.*,sales.*\",
-      \"topic.prefix\": \"source\",
-      \"snapshot.mode\": \"initial\",
-      \"tombstones.on.delete\": \"false\",
-      \"transforms\": \"unwrap\",
-      \"transforms.unwrap.type\": \"io.debezium.transforms.ExtractNewRecordState\"
-    }
-  }"
-echo "Debezium connector registered successfully."
-echo "Waiting for Debezium connector to start..."
+  -d @- <<EOF
+{
+  "name": "postgres-source-connector",
+  "config": {
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "plugin.name": "pgoutput",
+    "database.hostname": "sources",
+    "database.port": "5432",
+    "database.user": "$SRC_POSTGRES_USER",
+    "database.password": "$SRC_POSTGRES_PASSWORD",
+    "database.dbname": "$SRC_POSTGRES_DB",
+    "database.server.name": "pgserver",
+    "slot.name": "debezium",
+    "publication.name": "dbz_publication",
+    "table.include.list": "production.*,sales.*",
+    "topic.prefix": "source",
+    "snapshot.mode": "initial",
+    "tombstones.on.delete": "false",
+    "transforms": "unwrap",
+    "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+    "decimal.handling.mode": "string"
+  }
+}
+EOF
+
+echo "✅ Debezium connector registered successfully."
+echo "⏳ Waiting for Debezium connector to start..."
